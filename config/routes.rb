1410949428516c381acda7b6497b8a288d429d81
Sarah Sanders <jsanders.sarah@gmail.com>
2019-02-01 13:39:20 -0600

    User Story 3 CRUD Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :wiki
+
+  get 'about' => 'welcome#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'welcome#index'
 end
+
+#we call the resources method and pass it a Symbol. This instructs Rails to create post routes for creating, updating, viewing, and deleting instances of Post. We'll review the precise URIs created in a moment.
+#
