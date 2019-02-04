d25fbee2a68a418ab07032a809544e404e98be31
Sarah Sanders <jsanders.sarah@gmail.com>
2019-02-04 14:44:48 -0600

    Pundit & Roles Incomplete

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -4,11 +4,29 @@ class WikiController < ApplicationController
   end
 
   def show
+    @user = authorize User.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
+
+  def admin_list
+    authorize Wiki # we don't have a particular post to authorize
+    # Rest of controller action
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+    if @wiki.update(wiki_params)
+      redirect_to @wiki
+    else
+      render :edit
+  end
+end
 end
