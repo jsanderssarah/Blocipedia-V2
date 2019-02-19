a46b7bc15e9ea3ff774574f7ebb05e0a17df3fb1
Sarah Sanders <jsanders.sarah@gmail.com>
2019-02-19 10:07:17 -0600

    User Roels Updated

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,6 +1,7 @@
 class WikisController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @wikis = Wiki.all
+    @wikis = WikiPolicy::Scope.new(current_user, Wiki).resolve
   end
 
   def show
@@ -10,6 +11,20 @@ class WikisController < ApplicationController
   def new
     @wiki = Wiki.new
   end
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+    @wiki.user = current_user
+
+    if @wiki.save
+      flash[:notice] = "Wiki saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving your Wiki. Try again."
+      render :new
+    end
+  end
+
 
   def edit
     @wiki = Wiki.find(params[:id])
@@ -24,4 +39,21 @@ class WikisController < ApplicationController
       render :edit
   end
 end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "#{@wiki.title} was deleted."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "Delete not successful"
+      render :show
+    end
+
+    private
+    def wiki_params
+      params.require(:wiki).permit(:title, :body, :private)
+    end
+  end
 end
